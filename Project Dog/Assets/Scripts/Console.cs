073f8ed812c1da6c7164fa7913f04ccaa0665e17
Maurice <40207470+Eelazar@users.noreply.github.com>
2019-06-05 17:28:14 +0200

    Finished console log scroll

--- Project Dog/Assets/Scripts/Console.cs
+++ Project Dog/Assets/Scripts/Console.cs
@@ -9,14 +9,21 @@ public class Console : MonoBehaviour
     public TMP_InputField console_InputField;
     public TMP_Text[] log_TextFields;
 
-    public string focus;
+    private string focus;
+    private string subFocus;
+
+    private int currentLogIndex;
+    private int selectedSlotIndex;
+
+    public Color inputFadeColor;
 
     private string rawInput;
 
+    private string[] consoleLog = new string[50];
+
     void Start()
-    {        
-        focus = "console";
-        console_InputField.ActivateInputField();
+    {
+        SetFocus("input");
     }
 
     void Update()
@@ -26,30 +33,146 @@ public class Console : MonoBehaviour
 
     void GetInput()
     {
-        if(focus == "console" && Input.GetKeyUp(KeyCode.Return))
+        if(focus == "console" && subFocus == "input")
         {
-            rawInput = console_InputField.text;
-            console_InputField.text = "";
-
-            if(rawInput != "")
+            if (Input.GetKeyUp(KeyCode.Return))
             {
-                LogText(rawInput + "\n");
+                rawInput = console_InputField.text;
+                console_InputField.text = "";
+
+                if (rawInput != "")
+                {
+                    LogText(rawInput);
+                }
+
+                console_InputField.ActivateInputField();
             }
+
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                SetFocus("log");
+
+                ScrollText(true, true);
+            }
+        }
+        else if (focus == "console" && subFocus == "log")
+        {
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                ScrollText(true);
+            }
+
+            if(Input.GetKeyUp(KeyCode.DownArrow) && currentLogIndex == 0)
+            {
+                ScrollText(false, true);
+                SetFocus("input");
+            }
+            else if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                ScrollText(false);
+            }
+
+        }
+    }
+
+
+    void SetFocus(string f)
+    {
+        if (f == "input")
+        {
+            focus = "console";
+            subFocus = "input";
 
             console_InputField.ActivateInputField();
         }
+        else if (f == "log")
+        {
+            focus = "console";
+            subFocus = "log";
+
+            console_InputField.DeactivateInputField();
+        }
+    }
+
+    void ScrollText(bool up, bool special = false)
+    {
+        //First time scrolling up, e.g. switching from input to log
+        if (special && up)
+        {
+            currentLogIndex = 0;
+            selectedSlotIndex = 0;
+        }        
+        else if(up)
+        {
+            if(currentLogIndex >= 4)
+            {
+                //If selection is at the top of window, only scroll
+                currentLogIndex++;
+            }
+            else
+            {
+                //If selection is not at the top of window, also move selection
+                currentLogIndex++;
+                selectedSlotIndex++;
+            }
+        }
+        else if (!up)
+        {
+            if(currentLogIndex > 0)
+            {
+                //If log is above 0, scroll down
+                currentLogIndex--;
+            }
+
+            if(selectedSlotIndex > 0)
+            {
+                //if selection is not at the bottom, also move selection
+                selectedSlotIndex--;
+            }
+        }
+
+        UpdateLog();
+
+        //If we're switching from log to input, reset the input
+        if (special && !up)
+        {
+            console_InputField.text = "";
+        }
+        else 
+        {
+            //Add an indentation to currntly selected text
+            log_TextFields[selectedSlotIndex].text = "> " + log_TextFields[selectedSlotIndex].text;
+            //Set Input Text to selected text, also lower alpha
+            console_InputField.text = "<color=#" + ColorUtility.ToHtmlStringRGBA(inputFadeColor) + ">" + consoleLog[currentLogIndex] + "</color>";
+        }
+
     }
 
     void LogText(string s)
     {
+        //temp1 gets the new text
         string temp1 = s;
         string temp2;
 
+        for (int i = 0; i < consoleLog.Length; i++)
+        {
+            //Temp2 gets the old text
+            temp2 = consoleLog[i];
+            //Old text is replaced by the new text
+            consoleLog[i] = temp1;
+            //Old text is replaced by new text
+            temp1 = temp2;
+        }
+
+        UpdateLog();
+    }
+
+    void UpdateLog()
+    {
+        //Fill the text fields
         for (int i = 0; i < log_TextFields.Length; i++)
         {
-            temp2 = log_TextFields[i].text;
-            log_TextFields[i].text = temp1;
-            temp1 = temp2;
+            log_TextFields[i].text = consoleLog[currentLogIndex - selectedSlotIndex + i];
         }
     }
 }
