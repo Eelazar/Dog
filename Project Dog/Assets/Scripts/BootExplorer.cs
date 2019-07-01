e4a95833258014fd984f4acd816051d552486c34
Maurice <40207470+Eelazar@users.noreply.github.com>
2019-07-01 18:21:04 +0200

    Started implementing explorer in boot

---
+++ Project Dog/Assets/Scripts/BootExplorer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BootExplorer : MonoBehaviour
+{
+    private const float lineHeight = 20F;
+
+    public GameObject node_Prefab;
+    public GameObject explorerPanel;
+
+    private GameObject explorerWindow;
+    void Start()
+    {
+        explorerWindow = explorerPanel.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void ResizeLog()
+    {
+        //Measure how many pixels are unused (e.g. the top part of the window -> decoration)
+        float yOffset = explorerPanel.GetComponent<RectTransform>().sizeDelta.y;
+
+        //Get the anchors of the log window, i.e. from where to where on the screen it spans
+        Vector2 parentAnchorMin = new Vector2(explorerWindow.GetComponent<RectTransform>().anchorMin.x, explorerWindow.GetComponent<RectTransform>().anchorMin.y);
+        Vector2 parentAnchorMax = new Vector2(explorerWindow.GetComponent<RectTransform>().anchorMax.x, explorerWindow.GetComponent<RectTransform>().anchorMax.y);
+
+        //From the anchors get the percentage of screen the log window occupies
+        Vector2 parentScreenPercent = new Vector2(parentAnchorMax.x - parentAnchorMin.x, parentAnchorMax.y - parentAnchorMin.y);
+
+        //Get the height in pixels of the entire game window
+        float canvasHeight = Screen.height;
+
+        //Calculate the actual height of the log by multiplying the screen height by the window percentage, and finally substracting the unused space
+        float logSize = (canvasHeight * parentScreenPercent.y) + yOffset;
+
+        //Calculate the highest possible amount of slots that can fit into the log
+        int slotAmount = Mathf.FloorToInt(logSize / lineHeight);
+
+        //Check how many slots are currently active
+        int activeAmount = log_ActiveFields.Count;
+
+        Debug.Log("Pixel Height: " + canvasHeight + ", Log Window Size: " + logSize + ", Offset: " + yOffset + ", Slots: " + slotAmount + ", Active Slots: " + activeAmount);
+
+        //If more slots could fit into the log, add them
+        if (activeAmount < slotAmount)
+        {
+            for (int i = activeAmount; i < slotAmount; i++)
+            {
+                log_TextFields[i].gameObject.SetActive(true);
+                log_ActiveFields.Add(log_TextFields[i].gameObject);
+            }
+        }
+        //Otherwise remove the excess slots
+        else if (activeAmount > slotAmount)
+        {
+            for (int i = activeAmount; i > slotAmount; i--)
+            {
+                log_TextFields[i - 1].gameObject.SetActive(false);
+                log_ActiveFields.Remove(log_TextFields[i - 1].gameObject);
+            }
+
+            //Cleanup
+            for (int i = activeAmount; i < log_TextFields.Length; i++)
+            {
+                log_TextFields[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
