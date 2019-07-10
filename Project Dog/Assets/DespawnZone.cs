94795a7af953e31d52bad7328bb54dcfbde874a6
Jan <jan-wichmann@live.com>
2019-07-10 19:41:07 +0200

    DespawnZone

---
+++ Project Dog/Assets/DespawnZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnZone : MonoBehaviour
+{
+    public string tag;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals(tag))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
