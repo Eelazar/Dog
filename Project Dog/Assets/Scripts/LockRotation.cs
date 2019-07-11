65a6596fb04244ba224b34613dfbc7c23fdd19c0
Niklas Wirths <niklaswirths@yahoo.de>
2019-07-11 15:44:05 +0200

    More lvl1 stuff

---
+++ Project Dog/Assets/Scripts/LockRotation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LockRotation : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "trash")
+        {
+            other.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+}
