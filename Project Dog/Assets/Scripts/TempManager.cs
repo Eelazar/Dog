eeb6dd31e8da43658a6ee6d5717370793a43a3a5
Maurice <40207470+Eelazar@users.noreply.github.com>
2019-07-14 21:06:40 +0200

    Added temporary manager to lock cursor in console scene

---
+++ Project Dog/Assets/Scripts/TempManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TempManager : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        
+    }
+}
